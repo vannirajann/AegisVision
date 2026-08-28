a525e20885c0883276ffa38c6a2df0148bd6e56f
Pranathi <pranathi.21p@gmail.com>
2026-08-28 21:57:27 +0530

    Add LineCrossingDetector: detect the moment an object crosses the line

--- tracking-intrusion/fence.py
+++ tracking-intrusion/fence.py
@@ -32,3 +32,34 @@ class VirtualLine:
             return "right"
         else:
             return "on_line"
+class LineCrossingDetector:
+    """
+    Tracks which side of the line each object was on last frame,
+    so we can detect the exact moment an object crosses over.
+    """
+
+    def __init__(self, virtual_line):
+        self.line = virtual_line
+        self.last_side = {}  # track_id -> "left" or "right" (last known side)
+
+    def check_crossing(self, track_id, point):
+        """
+        Call this once per object, per frame, with its current point.
+        Returns True if this object just crossed the line THIS frame,
+        otherwise False.
+        """
+        current_side = self.line.side_of_line(point)
+
+        crossed = False
+
+        if track_id in self.last_side:
+            previous_side = self.last_side[track_id]
+            # Only count it as a crossing if both sides are valid (not "on_line")
+            # and they are DIFFERENT from each other.
+            if previous_side != current_side and previous_side != "on_line" and current_side != "on_line":
+                crossed = True
+
+        # Remember this side for next frame's comparison
+        self.last_side[track_id] = current_side
+
+        return crossed
